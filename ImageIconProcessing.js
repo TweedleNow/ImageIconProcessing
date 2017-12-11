0c69f29e5117c72e6851eb17c7a61e0feede87a0
Priyanck <priyank.kumar@tweedl.in>
2017-12-11 11:22:45 +0530

    refactored the imageProcessing

--- ImageIconProcessing.js
+++ ImageIconProcessing.js
@@ -5,10 +5,6 @@ var gm = require('gm')
     .subClass({ imageMagick: true }); // Enable ImageMagick integration.
 var util = require('util');
 
-// constants
-var MAX_WIDTH = 100;
-var MAX_HEIGHT = 100;
-
 // get reference to S3 client 
 var s3 = new AWS.S3();
 
@@ -17,13 +13,9 @@ exports.handler = function (event, context, callback) {
     console.log("Reading options from event:\n", util.inspect(event, { depth: 5 }));
     var srcBucket = event.Records[0].s3.bucket.name;
     // Object key may have spaces or unicode non-ASCII characters.
-    var srcKey =
-        decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+    var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
     var dstBucket = srcBucket + "resized";
     var dstKey = "resized-" + srcKey;
-    var dstKeyTwo = "resized-two-" + srcKey;
-    var dstKeyThree = "resized-three-" + srcKey;
-    var dstKeyFour = "resized-four-" + srcKey;
 
     // Sanity check: validate that source and destination are different buckets.
     if (srcBucket == dstBucket) {
@@ -43,7 +35,6 @@ exports.handler = function (event, context, callback) {
         return;
     }
 
-    //async functions
 
 
 
